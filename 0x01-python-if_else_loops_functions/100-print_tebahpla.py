c4fe5f7f43c60d70440a61604d6b37ffac5a6c20
Jesufemi-A <jesufemia1@gmail.com>
2023-06-07 12:17:08 -0700

    code outputting ascii chars in desdending order in alternating case

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(ord('z'), ord('a') - 1, -1):
+    if num % 2 == 0:
+        result = chr(num)
+    elif num % 2 != 0:
+        result = chr(num - 32)
+    print(result, end="")
+    
+

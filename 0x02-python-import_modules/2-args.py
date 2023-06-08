f692fca71ede927c019dd4f450071a173870b9c0
Jesufemi-A <jesufemia1@gmail.com>
2023-06-08 13:55:36 -0700

    importing sys module to print number and list of number

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    arguments = sys.argv[1:]
-    arg_num = len(arguments)
-    if arg_num == 0:
-        print("0 arguments.")
-    elif arg_num == 1:
-        print("1 arguments:")
+import sys
+
+def print_arguments(argv):
+    num_arguments = len(argv)
+    if num_arguments == 0:
+        print("0 argument(s).")
+        print(".")
+        return
+
+    if num_arguments == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(arguments))
-        for num, argument in enumerate(arguments):
-            print("{}: {}".format(num + 1, argument))
+        print("{} arguments:".format(num_arguments))
+
+    for i, arg in enumerate(argv, 1):
+        print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    print_arguments(sys.argv[1:])

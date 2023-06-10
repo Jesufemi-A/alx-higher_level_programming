c3a93e609d3cafa7ce4b5a439162a8b34f6820e3
Jesufemi-A <jesufemia1@gmail.com>
2023-06-10 05:56:01 -0700

    refactoring

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list is not None:
         list_copy = my_list[:]
-        if idx < 0 or idx > len(list_copy):
+        if idx < 0 or idx >= len(list_copy):
             return list_copy
         else:
             list_copy = list_copy[:idx] + list_copy[idx + 1:]

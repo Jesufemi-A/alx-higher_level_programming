68cecbf9b671d2def5048b42bc2492f531319f88
Jesufemi-A <jesufemia1@gmail.com>
2023-06-10 04:55:12 -0700

    refactor

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list is not None:
-        if idx < 0 or idx > len(my_list):
-            list_copy = my_list
+        list_copy = my_list
+        if idx < 0 or idx > len(list_copy):
             return list_copy
         else:
-            my_list = my_list[:idx] + my_list[idx + 1:]
-            my_list.insert(idx, element)
-            return my_list
+            list_copy = list_copy[:idx] + list_copy[idx + 1:]
+            list_copy.insert(idx, element)
+            return list_copy

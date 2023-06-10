dcd3e3fc125338a759baac428fe9bc2fdcd3d171
Jesufemi-A <jesufemia1@gmail.com>
2023-06-10 12:50:05 -0700

    added a functuon that works like pop() method

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list == []:
-        return None
-    elif idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     elif idx == 0:
         my_list = my_list[idx + 1:]

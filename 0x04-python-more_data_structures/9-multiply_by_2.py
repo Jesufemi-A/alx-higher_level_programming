f4d6c8daad1ddb6e53d3880d093e9ee73aa53d57
Jesufemi-A <jesufemia1@gmail.com>
2023-06-14 16:58:31 -0700

    multiplying value by 2 and returning a new dict

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    copy_dict = {}
+    copy_dict = dict(a_dictionary)
     for key, value in copy_dict.items():
         copy_dict[key] = value * 2
     return copy_dict

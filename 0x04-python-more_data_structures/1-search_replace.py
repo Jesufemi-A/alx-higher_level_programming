0ac865e2025c57c9f75f6e484fceccbb396e814c
Jesufemi-A <jesufemia1@gmail.com>
2023-06-14 06:24:43 -0700

    replacing item in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new = []
     for i in my_list:
-            if i == search:
-                new.append(replace)
-            else:
-                new.append(i)
+        if i == search:
+            new.append(replace)
+        else:
+            new.append(i)
     return new

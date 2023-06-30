bf80030e8f56b1de9ca3657593a739518fa39d97
Jesufemi-A <jesufemia1@gmail.com>
2023-06-29 17:41:22 -0700

    refactoring code meet checker criteria

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a class with private instance attribute - size
+"""
+
+
+class Square:
+    """
+    class with private instance attribute, size
+    """
+    def __init__(self, size):
+        """
+        special method for initialising attrihutes
+        """
+
+        self._size

b8db039d81b2c293a660931292d6fc19ca8385c9
Jesufemi-A <jesufemia1@gmail.com>
2023-06-29 19:42:19 -0700

    adding code that use getter and setter method to access and modify value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This is a module that define a class Square
+has validationfor parameter size of class Square
+size: size of the square
+
+uses the getter and setter method
+"""
+
+
+class Square:
+    """
+    This is a class Square
+    define the size of a square and has type and value validatio
+    """
+
+    def __init__(self, size=0):
+        """
+        A special method that initilize paramter size
+        checks for validation too
+        """
+
+        if isinstance(size, int) is False:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must >= 0')
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        returns the size of square using getter method
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        check for validation
+        use setter method to access and modify the value
+        """
+
+        if isinstance(value, int) is False:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        This method returns th area of a square
+        """
+
+        return self.__size * self.__size

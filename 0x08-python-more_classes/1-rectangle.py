c7339b0734bebfe1d8edbda3ef8ff5027f0aa0dc
Jesufemi-A <jesufemia1@gmail.com>
2023-07-03 15:14:05 -0700

    refactoring code to print right output

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -57,8 +57,8 @@ class Rectangle:
         """
 
         if not isinstance(value, int):
-            raise TypeError('Height must be an integer')
+            raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('Height must be >= 0')
+            raise ValueError('height must be >= 0')
         else:
             self.__height = value

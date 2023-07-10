fbc7cccfb8c408f94e62d33e5af94de1cf4c221e
Jesufemi-A <jesufemia1@gmail.com>
2023-07-11 00:28:33 +0100

    refactoring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,7 +25,7 @@ class BaseGeometry:
         else raise an exception
         """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

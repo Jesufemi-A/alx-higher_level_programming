56c2b50dd7544be76e17e248730a6c291bf1fea4
Jesufemi-A <jesufemia1@gmail.com>
2023-07-10 19:03:20 -0700

    printing sorted list

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,6 +16,7 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(self, width, height)
 
     def area(self):
         """

9b8e1d62262e29355df07783a4226191030b513b
Jesufemi-A <jesufemia1@gmail.com>
2023-07-11 16:59:42 -0700

    refactoring codes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
 
     with open(filename, "r", encoding="utf-8") as file_a:
         file_content = file_a.read()
-        return file_content
+        print(file_content, end="")

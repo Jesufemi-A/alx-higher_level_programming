5ca5556ce25db1fff03bdcf70cd698581e979aa7
Jesufemi-A <jesufemia1@gmail.com>
2023-07-10 17:54:40 -0700

    updating BaseGeometry class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A module containing a subclass
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A sub class inheriting from Baseclass
+    """
+
+    def __init__(self, width, height):
+        """
+        This intializes the instance attributes
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        calculate area
+        """
+
+        return self.__width * self.__height
+
+    def print(self):
+        """
+        print rectangle
+        """
+
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def __str__(self):
+        """
+        rectangle rectangle
+        """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

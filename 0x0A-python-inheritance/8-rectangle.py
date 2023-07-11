971763993538b71de2df6452c3df693fd52b72de
Jesufemi-A <jesufemia1@gmail.com>
2023-07-11 01:15:27 +0100

    refactoring

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A module containing a subclass
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A sub class inheriting from Baseclass
+    """
+
+    def __init__(self, width, height):
+        """
+        This intializes the instance attributes
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", width)
+        self.__width = width
+        self.__height = height
+
+
+
+
+

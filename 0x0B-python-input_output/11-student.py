eda4c97f9ce6cda3e52042365d984fd47cca612f
Jesufemi-A <jesufemia1@gmail.com>
2023-07-11 19:16:28 -0700

    replacing attribute in Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+module contain class student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        constructor
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        convert json
+        """
+
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                value = getattr(self, attr)
+                if isinstance(value, (list, dict, str, int, bool)):
+                    result[attr] = value
+        return result
+
+    def reload_from_json(self, json):
+        """
+        reload from json
+        """
+
+        self.__dict__ = json

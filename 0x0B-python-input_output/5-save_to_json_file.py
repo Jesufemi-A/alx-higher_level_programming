205bae60e48bf9136eb71b01b0cd66df89f4959a
Jesufemi-A <jesufemia1@gmail.com>
2023-07-11 17:16:09 -0700

    refactoring codes

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,7 +11,7 @@ def save_to_json_file(my_obj, filename):
     using JSON representation
     """
 
-    json_rep = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as file_w:
+        json_rep = json.dumps(my_obj)
         num_chars_written = file_w.write(json_rep)
         return num_chars_written

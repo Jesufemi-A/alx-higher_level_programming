48d08e8e222a2540961525030a5831b89b603f21
Jesufemi-A <jesufemia1@gmail.com>
2023-08-16 16:09:07 +0100

    create table table_name

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Generates the database, hbtn_0d_2 and the user, user_0d_2
+-- The user_0d_2 is granted SELECT priviledge on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+	IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+	IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+	ON `hbtn_0d_2`.*
+	TO 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Generates the table force_name in my server
+CREATE TABLE IF NOT EXISTS `force_name` 
+	`id`   INT,
+	`name` VARCHAR(256) NOT NULL
+);

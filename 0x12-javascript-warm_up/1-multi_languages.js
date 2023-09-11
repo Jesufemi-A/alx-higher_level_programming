a54cee077cfb9a9c89bbf90bc07041698a45823a
Jesufemi-A <jesufemia1@gmail.com>
2023-09-11 12:35:36 +0000

    refactoring code to meet requirement

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const c = "C is fun";
-const python = "Python is cool";
-const javascript = "JavaScript";
-console.log(c);
-console.log(python);
-console.log(javascript);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");

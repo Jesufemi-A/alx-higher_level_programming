cba51004bc20cc5a764a2eafc8722c212dc0c524
Jesufemi-A <jesufemia1@gmail.com>
2023-09-12 00:11:02 +0000

    refactoring

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,6 @@
 
 if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
+} else if (process.argv.length > 2) {
   console.log('Argument found');
 }

fe30c47c58a09aea5105dd76605269ad71683e2b
Jesufemi-A <jesufemia1@gmail.com>
2023-09-12 20:04:57 +0000

    added a method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      const emptyRectangle = Object.create(Rectangle.prototype);
+      return emptyRectangle;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; i++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+}
+
+module.exports = Rectangle;

35a6b6ec96a708201f2f150e9f814b35804dc9a3
Jesufemi-A <jesufemia1@gmail.com>
2023-09-12 00:39:15 +0000

    fixing and refactoring

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 
+const firstArgument = parseInt(process.argv[2]);
+
+if (!isNaN(firstArgument)) {
+  console.log(`My Number: ${firstArgument}`);
+} else {
+  console.log('Not a number');
+}

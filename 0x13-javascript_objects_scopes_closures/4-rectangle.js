eaf6430a7ccdd4345002559ec4e09d0cc89ddc66
Jesufemi-A <jesufemia1@gmail.com>
2023-09-13 09:51:45 +0000

    refactoring

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     let rect = '';
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
         rect += 'X';
       }
       console.log(rect);

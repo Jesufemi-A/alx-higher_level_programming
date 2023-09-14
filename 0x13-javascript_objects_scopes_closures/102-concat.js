a4ddb8898deb28747fec849a2a44a63ba749978b
Jesufemi-A <jesufemia1@gmail.com>
2023-09-14 00:00:06 +0000

    concat 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+if (args.length !== 3) {
+  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+  process.exit(1);
+}
+
+const sourceFile1 = args[0];
+const sourceFile2 = args[1];
+const destinationFile = args[2];
+
+try {
+  // Read the contents of the first source file
+  const data1 = fs.readFileSync(sourceFile1, 'utf8');
+
+  // Read the contents of the second source file
+  const data2 = fs.readFileSync(sourceFile2, 'utf8');
+
+  // Concatenate the contents of the two source files
+  const concatenatedData = data1 + data2;
+
+  // Write the concatenated data to the destination file
+  fs.writeFileSync(destinationFile, concatenatedData);
+
+  console.log(`Contents of ${sourceFile1} and ${sourceFile2} have been concatenated to ${destinationFile}`);
+} catch (error) {
+  console.error('Error:', error.message);
+  process.exit(1);
+}
